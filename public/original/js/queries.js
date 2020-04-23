016a0982fe077c8d4ac5e2d43e45f2cf22af7dac
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-04-23 13:40:08 +0530

    csp resolution attempt 3

--- public/original/js/queries.js
+++ public/original/js/queries.js
@@ -1,7 +1,5 @@
 var socket = io();
 
-document.getElementById("overlay").style.display = "block";
-
 var token = location.pathname.toString();
     token = token.replace('/chat/', '');
     token = token.replace('/', '');
